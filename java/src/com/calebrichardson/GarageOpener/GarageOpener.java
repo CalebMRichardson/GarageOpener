9874c8ee2083bbe4b84da9e5fe3604a0d95bcbab
Caleb <cmrichardson1993@gmail.com>
2019-01-02 03:23:03 +0000

    added program reading port number from file. This will be default behavior unless user runs program with params

--- java/src/com/calebrichardson/GarageOpener/GarageOpener.java
+++ java/src/com/calebrichardson/GarageOpener/GarageOpener.java
@@ -14,6 +14,7 @@ public class GarageOpener {
 
 	GarageDoor garageDoor = new GarageDoor();
 
+	
 	Runtime.getRuntime().addShutdownHook(new Thread() {
 		public void run() {
 		    GarageOpenerUtil.log(TAG, "Service has been interuptted...shutting down.");
@@ -21,16 +22,70 @@ public class GarageOpener {
 		}
 	    });
 	
-	// todo change this to read from String[] args and default to a stored json file
-	int port = 9999;
 
+	final String PORT_CMD = "/port";
+	
+	int port = -1;
+	
+	String currentDir = System.getProperty("user.dir");
+        String configFilePath = currentDir + "/../../config";
+
+	BufferedReader br = null;
+	String line = "";
+	String cvsSplit = ",";
+	
+	File configFile = new File(configFilePath); 
+
+	if (!configFile.exists()) {
+	    System.out.println("No config file.");
+	    port = 1993;
+	    //todo create a new config file give it default port; 
+	}
+	
+	try {
+
+	    br = new BufferedReader(new FileReader(configFile));
+
+	    while ((line = br.readLine()) != null) {
+		
+		String[] value = line.split(cvsSplit);
+		int result = Integer.parseInt(value[1]);
+		port = result;
+	    }
+
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+
+	    if (br != null) {
+		try {
+		    br.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+
+
+	if (port == -1) {
+	    System.out.println("Bad port");
+	    //todo handle error
+	    return;
+	}
+	runThread(port, garageDoor);
+
+
+    }
+
+    private static void runThread(int port, GarageDoor garageDoor) {
 	try {
 	    Thread t = new GarageOpenerServer(port, garageDoor);
 	    t.start();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-	
     }
     
 }
